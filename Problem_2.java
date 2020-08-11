5f37d3804d9455bb086affb7f93247889d7db4cc
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-08-11 13:54:19 -0700

    Create Problem_2.java

---
+++ Problem_2.java
@@ -0,0 +1,24 @@
+// Time complexity - O(n)
+// Space complexity - O(n)
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                st.push(')');
+            } else if(c == '{'){
+                st.push('}');
+            } else if(c == '['){
+                st.push(']');
+            }else{
+                if(st.isEmpty() || c != st.pop()){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
