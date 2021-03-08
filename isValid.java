7f74aa1e098c3ca1b1d1122102a1134883ab8995
SAI TEJA NAGAM <47976398+Stnagam@users.noreply.github.com>
2021-03-08 22:25:48 +0530

    Create isValid.java

---
+++ isValid.java
@@ -0,0 +1,29 @@
+/*time complexity: O(n)
+space complexity:O(n)
+Algorithm:
+whenveve opening bracket is encountred, push the opposite i.e. closing brakcet in stack.
+when closing bracket is encountered, then chekc if curr closing bracket mataches with stack.pop(), if not return false. Also chekc if stack is not empty.
+*/
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                stack.push(')');
+            }
+            else if(ch == '{'){
+                stack.push('}');
+                
+            }else if(ch == '['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || ch != stack.pop()){
+                    return false;
+                
+            }
+        }
+        return stack.isEmpty();
+    }
+}
