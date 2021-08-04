3f230f39b7f9b800f252a71d5d1541b8359d9ecd
Tanreet Dhaliwal <tdhali@uwo.ca>
2021-08-03 23:06:00 -0400

    updated stacks 2

--- ValidParenthesis.java
+++ ValidParenthesis.java
@@ -1,46 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
 
-        Stack<Character> stack = new Stack<>();
+        //O(n) time and space
+        Stack<Character> st = new Stack<>();
 
-        for (char c : s.toCharArray()) {
-
-            if (c == '(' || c == '{' || c == '[') {
-
-                stack.add(c);
-
-            } else {
-
-                if (stack.isEmpty()) return false;
-
-                char stackTop = stack.pop();
-
-                if (!checkEquality(stackTop, c)) {
-                    return false;
-                }
-            }
-
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else if (st.isEmpty() || st.pop() != c) return false;
         }
 
-        return stack.isEmpty();
-
-    }
-
-    private boolean checkEquality(Character open, Character c) {
-
-        if (open == '(' && c == ')') {
-            return true;
-        }
-
-        if (open == '[' && c == ']') {
-            return true;
-        }
-
-        if (open == '{' && c == '}') {
-            return true;
-        }
-
-        return false;
+        return st.isEmpty();
 
     }
 }
