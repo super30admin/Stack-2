5bda170fcf165f6b3184f394f12beca75f1ab149
Sai Sruthi Mareedu <smareed1@sundevils.asu.edu>
2021-10-12 08:28:32 -0700

    Completed

---
+++ validParenthesis.java
@@ -0,0 +1,23 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public boolean isValid(String s) {
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+}
+    }
