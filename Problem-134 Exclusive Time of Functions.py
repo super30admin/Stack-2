e3369b0b0e345c8aaad396804c3ac73b8ba29638
Adhish Shrivastava <adhish70@gmail.com>
2021-12-23 11:40:31 -0500

    Initial Commit

---
+++ Problem-134 Exclusive Time of Functions.py
@@ -0,0 +1,11 @@
+# 636. Exclusive Time of Functions
+# https://leetcode.com/problems/exclusive-time-of-functions/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
+        pass
---
+++ Problem-135 Valid Parentheses.py
@@ -0,0 +1,11 @@
+# 20. Valid Parentheses
+# https://leetcode.com/problems/valid-parentheses/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pass
