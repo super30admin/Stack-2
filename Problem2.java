10070344a509cd70109f5e4a70a2ee984bf07e3b
Rujal <rujal8452@gmail.com>
2022-06-09 01:10:15 -0400

    Problem2

---
+++ Problem2.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Problem2 {
+    // TC: O(n)
+    // SC: O(n)
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) return true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+}
