5c3a3e044f6106a5a5727f4389009ab0a1a1e6f1
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-03 09:51:19 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,55 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+//******135.20. VALID PARANTHESIS*****
+// Time Complexity :O(N);
+// Space Complexity :O(1);
+// Did this code successfully run on Leetcode :Y;
+// Any problem you faced while coding this :N;
 
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        if(s.length()==0) return true;
+        
+        //If string is odd, for sure it is not the valid paranthesis
+        if(s.length()%2!=0) return false;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            
+            if(c=='(')
+            {
+                st.push(')');
+            }
+            
+            else if(c=='[')
+            {
+                st.push(']');
+            }
+            else if(c=='{')
+            {
+                st.push('}');
+            }
+            else
+            {
+                if(st.isEmpty() || st.pop()!=c)
+                {
+                    return false;
+                }
+            }
+            
+            
+        }
+        
+        //Lastly still if stack is not empty that means not valid.
+        if(!st.isEmpty())
+        {
+            return false;
+        }
+        return true;
+        
+        
+    }
+}
 
 // Your code here along with comments explaining your approach
