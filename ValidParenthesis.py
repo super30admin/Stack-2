46af6084b83ee1eb6054f84de7d62df94d1932c6
tathyapatel <92098849+tathyapatel@users.noreply.github.com>
2022-08-03 19:11:12 -0500

    Create ValidParenthesis.py

---
+++ ValidParenthesis.py
@@ -0,0 +1,26 @@
+#20. Valid Parentheses
+"""
+Time Compelxity : O(n)
+Space Complexity : O(n)
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #if len(s) % 2 != 0:
+        #    return False
+        
+        st = []
+        for i in s:
+            if i == '(':
+                st.append(')')
+                
+            elif i == '{':
+                st.append('}')
+                
+            elif i == '[':
+                st.append(']')
+                
+            elif len(st) == 0 or st.pop() != i:
+                return False
+            
+        if len(st) == 0:
+            return True
