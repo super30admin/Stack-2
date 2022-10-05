7298750eb6bf301e08e2ca1d2dfefb58261719df
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-10-04 23:47:21 -0400

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,17 @@
+//TC : O(n)
+//SC : O(n)
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0) return true;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c == '(') stack.push(')');
+            else if(c == '[') stack.push(']');
+            else if(c == '{') stack.push('}');
+            else if(stack.isEmpty() || c != stack.pop()) return false;
+        }
+        return stack.isEmpty();
+    }
+}
