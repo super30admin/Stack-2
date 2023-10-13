8f95213104273746eb8344f9d4633c8524a7a3e8
sharvanitamaskar <sharvanitamaskar@gmail.com>
2023-10-13 12:45:18 -0400

    Completed Stack-2

---
+++ Valid_Parenthesis.java
@@ -0,0 +1,25 @@
+//TC:o(n)
+//SC:o(n)
+
+import java.util.Stack;
+public class ValidParenthesis {
+	public boolean isValid(String s) {
+        int length = s.length();
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<length;i++){
+            char bracket = s.charAt(i);
+            if(bracket == '{'){
+                stack.push('}');
+            }
+            else if(bracket == '['){
+                stack.push(']');
+            }
+            else if(bracket == '('){
+                stack.push(')');
+            }
+            else if(stack.isEmpty() || stack.peek()!=bracket) return false;
+            else if (stack.peek()==bracket) stack.pop();
+        }
+        return stack.isEmpty();
+    }
+}
