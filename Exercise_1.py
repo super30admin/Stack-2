4f70e472b4f78abd7524a51ae14020520fd57ed8
deeksha <9107dt@gmail.com>
2023-10-16 00:36:33 -0400

    Stack-2 Complete

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,8 +1,8 @@
 # Problem1 Exclusive Time of Functions (https://leetcode.com/problems/exclusive-time-of-functions/)
-# // Time Complexity :
-# // Space Complexity :
-# // Did this code successfully run on Leetcode :
-# // Any problem you faced while coding this :
+# // Time Complexity : O(n)
+# // Space Complexity : O(n)
+# // Did this code successfully run on Leetcode :Yes
+# // Any problem you faced while coding this :No
 
 
 # // Your code here along with comments explaining your approach
